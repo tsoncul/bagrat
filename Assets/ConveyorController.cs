c72679bfc2edf02abbd9965d9c2ec5bde0b49374
Tuna Soncul <tsoncul@gmail.com>
2018-05-27 22:35:12 +0300

    Conveyor belt first implementation

---
+++ Assets/ConveyorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorController : MonoBehaviour {
+
+    public float conveyorSpeed = 1.0f;
+    public bool isEnabled = true;
+
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (isEnabled)
+        {
+            Vector3 movement = rb.transform.forward * conveyorSpeed * Time.deltaTime;
+            rb.position -= movement;
+            rb.MovePosition(rb.position + movement);
+        }
+    }
+}
