c4af4adae07918091631af58c74433ad34ce267c
Tuna Soncul <tsoncul@gmail.com>
2018-06-01 00:16:10 +0300

    Airport Controller first pass.
    Game time

---
+++ Assets/AirportController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AirportController : MonoBehaviour {
+
+    // Timekeeping
+    private System.DateTime time;
+    private float gameTimeScale = 60f;
+
+    public DateTime CurrentTime
+    {
+        get
+        {
+            return time;
+        }
+
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        // Initialize the Game Time to 8 AM, some day. This may be updated by the 
+        // game level in the future.
+        time = new System.DateTime(2020, 4, 12, 8, 0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Update game time
+        time = time.AddSeconds(Time.deltaTime * gameTimeScale);
+	}
+
+
+
+    //// Flight class that keeps track of a single flight and its luggage.
+    //public class Flight
+    //{
+    //    public DateTime departureTime;
+    //    public string destination;
+
+    //    LuggageController.Luggage[] luggagesInFlight;
+
+    //    public Flight(string destination, DateTime date)
+    //    {
+    //        this.destination = destination;
+    //        this.departureTime = date;
+
+
+    //    }
+    //}
+}
