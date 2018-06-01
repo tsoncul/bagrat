1b91336bb3770924be7c72c848f007e7db07a149
Tuna Soncul <tsoncul@gmail.com>
2018-06-01 22:40:58 +0300

    Game logic updates

--- Assets/Game Logic Scripts/AirportController.cs
+++ Assets/Game Logic Scripts/AirportController.cs
@@ -19,12 +19,21 @@ public class AirportController : MonoBehaviour {
 
     }
 
+    // Flights Register
+    private List<Flight> dailyFlights;
 
     // Use this for initialization
     void Start () {
         // Initialize the Game Time to 8 AM, some day. This may be updated by the 
         // game level in the future.
         time = new System.DateTime(2020, 4, 12, 8, 0, 0, 0);
+
+        // Initialize the daily flights list
+        dailyFlights = new List<Flight>();
+        FlightManager.PopulateDailyFlights(dailyFlights);
+
+        Debug.Log(dailyFlights.Count);
+        
 	}
 	
 	// Update is called once per frame
--- Assets/Game Logic Scripts/FlightManager.cs
+++ Assets/Game Logic Scripts/FlightManager.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public static class FlightManager {
 
+    // List of airports. This will be replaced by a better solution.
+    private static string[] airportList = {"ARN","MEX","CDG","JFK","DFW","HKG","MAD","BCN","FCO","LHR","IST","LAX","ICN","SIN","MUC","FRA","BRU","LIS","LUX" };
+    public static void PopulateDailyFlights(List<Flight> flights)
+    {
+        for (int i = 0; i < 10;i++)
+        {
+            string randDest = airportList[UnityEngine.Random.Range(0,airportList.Length - 1)];
+            DateTime randTime = new DateTime(2020, 4, 12, UnityEngine.Random.Range(8, 16), UnityEngine.Random.Range(0, 12) * 5, 0);
 
+            flights.Add(new Flight(randDest, randTime));
+        }
+    }
     
 }
 
